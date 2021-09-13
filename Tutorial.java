e48a39d9ba4104185b2a736d9b9dd93d28100037
jzl6309 <67445368+jzl6309@users.noreply.github.com>
2021-09-12 20:58:48 -0400

    My first commit

--- Tutorial.java
+++ Tutorial.java
@@ -2,6 +2,8 @@ public class Tutorial {
   public static void main(String[] args) {
   
     System.out.println("My first repository tutorial");
+    
+    System.out.println("I made new changes");
   
   }
 }
